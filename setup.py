1ba4c8f630a04f7ed985396bc73ece027635fce4
Jack Kenzakowski <jkenzakowski@gmail.com>
2024-07-02 12:39:18 -0400

    HW9 Finish

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='BookLovers',
       version='0.1',
       description='Book listing',
-      url='',
+      url='https://github.com/jkenzak/HW9',
       author='Jack Kenzakowski',
       author_email='jak5je@virginia.edu',
       license='MIT',
-      packages=['booklover', 'booklover_test'])
+      packages=['BookLovers'])
